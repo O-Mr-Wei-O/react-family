be6d44811d5fb6c308bbde63d0acfc88fc3cd2e5
O-Mr-Wei-O <Sinpo96@163.com>
2018-03-04 19:24:17 +0800

    修改了登录注册表单，使用了ant的组件

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,27 +1,49 @@
 import React, {Component} from 'react';
 import './Login.css';
-import { Input } from 'antd';
-import { Button } from 'antd';
+import {Form, Icon, Input, Button, Checkbox} from 'antd';
+import {Link} from 'react-router-dom';
+const FormItem = Form.Item;
 
-class Login extends React.Component {
+
+class LoginForm extends React.Component {
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    };
+
     render() {
+        const {getFieldDecorator} = this.props.form;
         return (
-            <div className={'Login'}>
-                <div className={'Login_container'}>
-                    {/*涉及到用户信息要用post*/}
-                    <form action="" method="post" target="nm_iframe">
-                        <Input placeholder={'用户名'}/>
-                        <br/>
-                        <Input placeholder={'密码'}/>
-                        <br/>
-                        <Button type="primary" htmlType={'submit'}>登录</Button>
-                    </form>
-                    {/*加上iframe使得form只提交不跳转*/}
-                    <iframe id="id_iframe" name="nm_iframe" style={{display:'none'}}/>
-                </div>
-            </div>
+            <Form onSubmit={this.handleSubmit} className="login-form" id={'components-form-demo-normal-login'}>
+                <FormItem>
+                    {getFieldDecorator('userName', {
+                        rules: [{required: true, message: 'Please input your username!'}],
+                    })(
+                        <Input prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>} placeholder="Username"/>
+                    )}
+                </FormItem>
+                <FormItem>
+                    {getFieldDecorator('password', {
+                        rules: [{required: true, message: 'Please input your Password!'}],
+                    })(
+                        <Input prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>} type="password"
+                            placeholder="Password"/>
+                    )}
+                </FormItem>
+                <FormItem>
+                    <Button type="primary" htmlType="submit" className="login-form-button">
+                        Login
+                    </Button>
+                    Or <Link to="/Register">register now!</Link>
+                </FormItem>
+            </Form>
         );
     }
 }
 
+const Login = Form.create()(LoginForm);
 export default Login;
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,29 +1,174 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './Register.css';
-import { Input } from 'antd';
-import { Button } from 'antd';
+import {Link} from 'react-router-dom';
 
-class Register extends React.Component {
+import {Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete} from 'antd';
+
+const FormItem = Form.Item;
+const Option = Select.Option;
+const AutoCompleteOption = AutoComplete.Option;
+
+
+class RegisterFrom extends React.Component {
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    };
+
+    state = {
+        confirmDirty: false,
+        autoCompleteResult: [],
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    };
+    handleConfirmBlur = (e) => {
+        const value = e.target.value;
+        this.setState({confirmDirty: this.state.confirmDirty || !!value});
+    };
+    checkPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && value !== form.getFieldValue('password')) {
+            callback('Two passwords that you enter is inconsistent!');
+        } else {
+            callback();
+        }
+    };
+    checkConfirm = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && this.state.confirmDirty) {
+            form.validateFields(['confirm'], {force: true});
+        }
+        callback();
+    };
+
     render() {
-        return (
-            <div className={'Register'}>
-                <div className={'Register_container'}>
-                    {/*涉及到用户信息要用post*/}
-                    <form action="" method="post" target="nm_iframe2">
-                        <Input placeholder={'用户名'}/>
-                        <br/>
-                        <Input placeholder={'密码'}/>
-                        <br/>
-                        <Input placeholder={'再次输入密码'}/>
-                        <br/>
-                        <Button type="primary" htmlType={'submit'}>注册</Button>
-                    </form>
-                    {/*加上iframe使得form只提交不跳转*/}
-                    <iframe id="id_iframe2" name="nm_iframe2" style={{display:'none'}}/>
-                </div>
-            </div>
+
+        const {getFieldDecorator} = this.props.form;
+        const {autoCompleteResult} = this.state;
+
+        const formItemLayout = {
+            labelCol: {
+                xs: {span: 24},
+                sm: {span: 8},
+            },
+            wrapperCol: {
+                xs: {span: 24},
+                sm: {span: 16},
+            },
+        };
+        const tailFormItemLayout = {
+            wrapperCol: {
+                xs: {
+                    span: 24,
+                    offset: 0,
+                },
+                sm: {
+                    span: 16,
+                    offset: 8,
+                },
+            },
+        };
+        const prefixSelector = getFieldDecorator('prefix', {
+            initialValue: '86',
+        })(
+            <Select style={{width: 70}}>
+                <Option value="86">+86</Option>
+            </Select>
         );
+
+        const websiteOptions = autoCompleteResult.map(website => (
+            <AutoCompleteOption key={website}>{website}</AutoCompleteOption>
+        ));
+
+
+        return (
+            <Form onSubmit={this.handleSubmit} id={'register-form'}>
+                <FormItem
+                    {...formItemLayout}
+                    label="E-mail"
+                >
+                    {getFieldDecorator('email', {
+                        rules: [{
+                            type: 'email', message: 'The input is not valid E-mail!',
+                        }, {
+                            required: true, message: 'Please input your E-mail!',
+                        }],
+                    })(
+                        <Input/>
+                    )}
+                </FormItem>
+                <FormItem
+                    {...formItemLayout}
+                    label="Password"
+                >
+                    {getFieldDecorator('password', {
+                        rules: [{
+                            required: true, message: 'Please input your password!',
+                        }, {
+                            validator: this.checkConfirm,
+                        }],
+                    })(
+                        <Input type="password"/>
+                    )}
+                </FormItem>
+                <FormItem
+                    {...formItemLayout}
+                    label="Confirm Password"
+                >
+                    {getFieldDecorator('confirm', {
+                        rules: [{
+                            required: true, message: 'Please confirm your password!',
+                        }, {
+                            validator: this.checkPassword,
+                        }],
+                    })(
+                        <Input type="password" onBlur={this.handleConfirmBlur}/>
+                    )}
+                </FormItem>
+                <FormItem
+                    {...formItemLayout}
+                    label={(
+                        <span>
+              Nickname&nbsp;
+                            <Tooltip title="What do you want others to call you?">
+                                <Icon type="question-circle-o"/>
+                            </Tooltip>
+                        </span>
+                    )}
+                >
+                    {getFieldDecorator('nickname', {
+                        rules: [{required: true, message: 'Please input your nickname!', whitespace: true}],
+                    })(
+                        <Input/>
+                    )}
+                </FormItem>
+                <FormItem
+                    {...formItemLayout}
+                    label="Phone Number"
+                >
+                    {getFieldDecorator('phone', {
+                        rules: [{required: true, message: 'Please input your phone number!'}],
+                    })(
+                        <Input addonBefore={prefixSelector} style={{width: '100%'}}/>
+                    )}
+                </FormItem>
+                <FormItem {...tailFormItemLayout}>
+                    <Button type="primary" htmlType="submit">Register</Button>
+                </FormItem>
+            </Form>);
     }
 }
 
+const Register = Form.create()(RegisterFrom);
 export default Register;
